fe0002dbeee195af1d1d1d6b7b5248c39182ef62
DallasWmk <dallasawomack@gmail.com>
2024-03-02 14:07:49 -0800

    initial commit

---
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,29 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+        "mypy",
+        "ruff",
+        "black",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+}
+
+return plugins
